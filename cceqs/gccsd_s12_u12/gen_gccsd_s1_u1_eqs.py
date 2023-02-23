081d694ba3c01315df90a4c8ea92233ab62526d9
Junjie Yang <yangjunjie0320@gmail.com>
2023-02-22 23:28:09 -0800

    update

--- cceqs/gccsd_s12_u12/gen_gccsd_s1_u1_eqs.py
+++ cceqs/gccsd_s12_u12/gen_gccsd_s1_u1_eqs.py
@@ -90,43 +90,84 @@ HTT   = commute(HT, T)
 HTTT  = commute(HTT, T)
 HTTTT = commute(HTTT, T)
 
-with open("gccsd_s1_u1_eqs.py", "w") as f:
-    print("Generating gccsd_s1_u1_eqs.py ...")
-    print("- energy equation")
-    S = (H + HT + Fraction('1/2')*HTT + Fraction('1/6')*HTTT)
-    out = apply_wick(S)
-    out.resolve()
-    final = AExpression(Ex=out)
-    gen_einsum_fxn(final, name_str="gccsd_s1_u1_ene", file_obj=f)
+def gen_gccsd_eom_ip_eqs():
+    from fractions import Fraction
+    from wick.convenience import one_e, two_e
+    from wick.convenience import E1, E2
+    from wick.convenience import Eip1, Eip2
+    from wick.convenience import braE1, braE2
+    from wick.convenience import braEip1, braEip2
 
-    print("- r1e equations")
-    bra = braE1("occ", "vir")
-    S = bra * (H + HT + Fraction('1/2')*HTT + Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT)
-    out = apply_wick(S)
-    out.resolve()
-    final = AExpression(Ex=out)
-    gen_einsum_fxn(final, name_str="gccsd_s1_u1_r1e", file_obj=f)
+    T1 = E1("t1e", ["occ"], ["vir"])
+    T2 = E2("t2e", ["occ"], ["vir"])
+    T  = T1 + T2
 
-    print("- r2e equations")
-    bra = braE2("occ", "vir", "occ", "vir")
-    S = bra * (H + HT + Fraction('1/2')*HTT + Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT)
-    out = apply_wick(S)
-    out.resolve()
-    final = AExpression(Ex=out)
-    gen_einsum_fxn(final, name_str="gccsd_s1_u1_r2e", file_obj=f)
+    R1 = Eip1("r1e", ["occ"])
+    R2 = Eip2("r2e", ["occ"], ["vir"])
+    R  = R1 + R2
 
-    print("- r1p equations")
-    bra = braP1("nm")
-    S = bra * (H + HT + Fraction('1/2')*HTT + Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT)
-    out = apply_wick(S)
-    out.resolve()
-    final = AExpression(Ex=out)
-    gen_einsum_fxn(final, name_str="gccsd_s1_u1_r1p", file_obj=f)
+    HT1 = commute(H,   T)
+    HT2 = commute(HT1, T)
+    HT3 = commute(HT2, T)
+    HT4 = commute(HT3, T)
 
-    print("- r1p1e equations")
-    bra = braP1E1("nm", "occ", "vir")
-    S = bra * (H + HT + Fraction('1/2')*HTT + Fraction('1/6')*HTTT + Fraction('1/24')*HTTTT)
-    out = apply_wick(S)
-    out.resolve()
-    final = AExpression(Ex=out)
-    gen_einsum_fxn(final, name_str="gccsd_s1_u1_r1p1e", file_obj=f)
+    hbar  = H + HT1 
+    hbar += Fraction('1/2')  * HT2
+    hbar += Fraction('1/6')  * HT3
+    hbar += Fraction('1/24') * HT4
+
+    expr    = H + HT1 + Fraction('1/2') * HT2
+    ene_cor = apply_wick(expr)
+    ene_cor.resolve()
+
+    def gen_einsum_fxn(final, name_str="get", return_str="res", arg_str_list=None, file_obj=None):
+        import cceqs
+        from cceqs.utils import space_idx_formatter
+        from cceqs.utils import einsum_str_formatter
+        from cceqs.utils import PYTHON_FILE_TAB
+
+        if arg_str_list is None:
+            arg_str_list = ["h1e", "h2e", "t1e", "t2e", "r1e", "r2e"]
+
+        arg_str = ""
+        for iarg, arg in enumerate(arg_str_list):
+            arg_str += f"{arg}, " if iarg != len(arg_str_list) - 1 else f"{arg}"
+
+        function_str  = '''\nfrom numpy import einsum\n\n'''
+        function_str += f"def {name_str}({arg_str}):\n"
+        function_str += PYTHON_FILE_TAB + "%s = 0.0\n" % return_str
+
+        einsum_str = final._print_einsum(
+            return_str, exprs_with_space=[H],
+            space_idx_formatter=space_idx_formatter, 
+            einsum_str_formatter=einsum_str_formatter
+            )
+        einsum_str = einsum_str.split("\n")
+
+        for i, line in enumerate(einsum_str):
+            function_str += f"{PYTHON_FILE_TAB}{line}\n"
+
+        function_str += f"{PYTHON_FILE_TAB}return res\n"
+
+        file_obj.write(function_str)
+        file_obj.write("\n\n")
+
+    with open("_gccsd_eom_ip_eqs.py", "w") as f:
+        print("Generating _gccsd_eom_ip_eqs.py ...")
+
+        print("- eom_ip_h1e equations")
+        bra = braEip1("occ")
+        expr = bra * (hbar - ene_cor) * R
+        out = apply_wick(expr)
+        out.resolve()
+        final = AExpression(Ex=out)
+        gen_einsum_fxn(final, name_str="gccsd_eom_ip_h1e", file_obj=f)
+
+        print("- eom_ip_h2e equations")
+        bra = braEip2("occ", "occ", "vir")
+        expr = bra * (hbar - ene_cor) * R
+        out = apply_wick(expr)
+        out.resolve()
+        final = AExpression(Ex=out)
+        gen_einsum_fxn(final, name_str="gccsd_eom_ip_h2e", file_obj=f)
+
